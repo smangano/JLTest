bb16c954875f76edab4a982c7c98782c076cd01b
Sal Mangano <sal31415@icloud.com>
2014-10-28 01:00:27 -0400

    Initial Version

--- src/JLTest.jl
+++ src/JLTest.jl
@@ -1,5 +1,357 @@
 module JLTest
 
-# package code goes here
+_TESTCTX = :(testContext)
 
-end # module
+type TestContext
+  name             #test case name
+  curTest          #current test
+  numRun           #num tests run
+  numPassed        #num tests passed
+  numFailed        #num assertions failed
+  numErrors        #num assertions with errors
+  numSkipped       #num tests skipped
+  setUpCase        #before test case
+  tearDownCase     #after test case
+  setUp            #before test
+  tearDown         #after test
+  preAssert        #before assertion
+  postAssert       #after assertion
+
+  TestContext(desc) = new (desc,"",0,0,0,0,0,()->nothing, ()->nothing, ()->nothing, ()->nothing,(args...)->nothing, (args...)->nothing)
+end
+
+function printTestReport(tc::JLTest.TestContext)
+  local rep = "Run: $(tc.numRun) | Passed: $(tc.numPassed) | Failed: $(tc.numFailed) | Errors: $(tc.numErrors) | Skipped: $(tc.numSkipped)"
+  local topLeft = repeat("=",int((length(rep) - length(tc.name) - 1)/2))
+  local topRight = repeat("=", length(rep) - length(topLeft) - length(tc.name) - 1)
+  local borderBot = repeat("=",length(rep))
+  println(topLeft," ",tc.name," ", topRight)
+  println(rep)
+  println(borderBot)
+end
+
+function setUp(tc::JLTest.TestContext)
+  try
+    tc.numRun += 1
+    tc.setUp()
+  catch ex
+    println("Exception in setUp for ", tc.desc, " : ", ex)
+  end
+end
+
+function doTest(tc::JLTest.TestContext, assertion::String, test::Function, args...)
+  if !(test(args...))
+    tc.numFailed += 1
+    print(assertion,"(")
+    for arg in args[1:end-1]
+      print(arg,", ")
+    end
+    println(args[end], ") failed")
+  end
+end
+
+function handleException(tc::JLTest.TestContext, assertion, ex, bt)
+  tc.numErrors += 1
+  println("Exception:", ex, " during ", assertion)
+  Base.show_backtrace(STDOUT, bt)
+end
+
+function tearDown(tc::JLTest.TestContext)
+  try
+    tc.tearDown()
+  catch ex
+    println("Exception in tearDown for ", tc.desc, " : ", ex)
+  end
+end
+
+
+macro assertion1(assertion,arg1,test)
+  quote
+    local tc = $(esc(_TESTCTX))
+    local a = $(esc(arg1))
+    tc.preAssert(tc, $assertion,a)
+    try
+
+      doTest(tc, $assertion, $(esc(test)),a)
+    catch ex
+      bt=catch_backtrace()
+      handleException(tc, $assertion, ex,bt)
+    end
+    tc.postAssert(tc, $assertion,a)
+  end
+end
+
+macro assertion2(assertion,arg1,arg2,test)
+  quote
+    local tc = $(esc(_TESTCTX))
+    local a = $(esc(arg1))
+    local b = $(esc(arg2))
+    tc.preAssert(tc, $assertion, a, b)
+    try
+      doTest(tc, $assertion, $(esc(test)),a,b)
+    catch ex
+      bt=catch_backtrace()
+      handleException(tc, $assertion, ex,bt)
+    end
+    tc.postAssert(tc, $assertion, a, b)
+  end
+end
+
+export @casename
+macro casename(str)
+  quote
+    $(esc(_TESTCTX)).name = $str
+  end
+end
+
+
+export @testname
+macro testname(str)
+  quote
+    $(esc(_TESTCTX)).curTest = $str
+  end
+end
+
+export @setUp
+#set a function to be called before each test starts
+macro setUp(func)
+  quote
+    $(esc(_TESTCTX)).setUp =  $(esc(func))
+    nothing
+  end
+end
+
+export @tearDown
+#set a function to be called after each test finishes
+macro tearDown(func)
+  quote
+    $(esc(_TESTCTX)).tearDown =  $(esc(func))
+    nothing
+  end
+end
+
+export @assertEqual
+macro assertEqual(val1,val2)
+  test = :((a,b)->(a == b))
+  :( @assertion2("assertEqual", $(esc(val1)), $(esc(val2)), $(esc(test))) )
+end
+
+export @assertNotEqual
+macro assertNotEqual(val1,val2)
+  test = :((a,b)->(a != b))
+  :(@assertion2("assertNotEqual",$(esc(val1)),$(esc(val2)),$(esc(test))))
+end
+
+export @assertLess
+#val1 < val2
+macro assertLess(val1,val2)
+  test = :((a,b)->(a < b))
+  :(@assertion2("assertEqual",$(esc(val1)),$(esc(val2)),$(esc(test))))
+end
+
+export @assertLessEqual
+#val1 <= val2
+macro assertLessEqual(val1,val2)
+  test = :((a,b)->(a <= b))
+  :(@assertion2("assertLessEqual",$(esc(val1)),$(esc(val2)),$(esc(test))))
+end
+
+export @assertGreater
+#val1 > val2
+macro assertGreater(val1,val2)
+  test = :((a,b)->(a > b))
+  :(@assertion2("assertGreater",$(esc(val1)),$(esc(val2)),$(esc(test))))
+end
+
+export @assertGreaterEqual
+#val1 >= val2
+macro assertGreaterEqual(val1,val2)
+  test = :((a,b)->(a >= b))
+  :(@assertion2("assertGreater",$(esc(val1)),$(esc(val2)),$(esc(test))))
+end
+
+export @assertIs
+#an obj1 is the same as obj2
+macro assertIs(obj1,obj2)
+  test = :((a,b)->(a === b))
+  :(@assertion2("assertIs",$(esc(obj1)),$(esc(obj2)),$(esc(test))))
+end
+
+export @assertIsNot
+#an obj1 is not the same as obj2
+macro assertIsNot(obj1,obj2)
+  test = :((a,b)->(a !== b))
+  :(@assertion2("assertIsNot",$(esc(obj1)),$(esc(obj2)),$(esc(test))))
+end
+
+export @assertIn
+#an obj not in collection
+macro assertIn(obj,collection)
+  test = :((a,b)->(a in b))
+  :(@assertion2("assertIn",$(esc(obj)),$(esc(collection)),$(esc(test))))
+end
+
+export @assertNotIn
+#an obj not in a collection
+macro assertNotIn(obj,collection)
+  test = :((a,b)->!(a in b))
+  :(@assertion2("assertNotIn",$(esc(obj)),$(esc(collection)),$(esc(test))))
+end
+
+export @assertItemsEqual
+#two collections equal ignoring order difference
+macro assertItemsEqual(col1,col2)
+  test = :((a,b)->(sort(a) == sort(b)))
+  :(@assertion2("assertItemsEqual",$(esc(col1)),$(esc(col2)),$(esc(test))))
+end
+
+export @assertIsA
+#an obj is a type
+macro assertIsA(obj,typ)
+  test = :((a,b)->isa(a,b))
+  :(@assertion2("assertIsA",$(esc(obj)),$(esc(typ)),$(esc(test))))
+end
+
+export @assertIsNotA
+#an obj is not a type
+macro assertIsNotA(obj,typ)
+  test = :((a,b)->!isa(a,b))
+  :(@assertion2("assertIsNotA", $(esc(obj)),$(esc(typ)), $(esc(test))))
+end
+
+export @assertMatches
+#a regex matches a string
+macro assertMatches(regex,str)
+  test = :((a,b)->ismatch(a,b))
+  :(@assertion2("assertMatches", $(esc(regex)),$(esc(str)),$(esc(test))))
+end
+
+export @assertNotMatches
+macro assertNotMatches(regex,str)
+  test = :((a,b)->!ismatch(a,b))
+  :(@assertion2("assertNotMatches", $(esc(regex)),$(esc(str)),$(esc(test))))
+end
+
+
+export @assertTrue
+macro assertTrue(expr)
+  test = :((a)->(isa(a,Bool) && a))
+  :(@assertion1("assertTrue", $(esc(expr)),$(esc(test))))
+end
+
+export @assertFalse
+macro assertFalse(expr)
+  test = :((a)->(isa(a,Bool) && !a))
+  :(@assertion1("assertFalse", $(esc(expr)),$(esc(test))))
+end
+
+export @testFailed(msg)
+#Fail and print msg
+macro testFailed(msg)
+  test = :((a)->false)
+  :(@assertion1("Failed",$(esc(msg)),$(esc(test))))
+end
+
+export @expectFailures
+#Use to assert that n failures are expected at the point where macro appears
+#and deduct from failure count
+macro expectFailures(n)
+  quote
+    if $(esc(_TESTCTX)).numFailed != $(esc(n))
+      println("Error: ", $(esc(n))," expected failures but ", $(esc(_TESTCTX)).numFailed, " actual")
+    else
+      $(esc(_TESTCTX)).numFailed -= $(esc(n))
+    end
+  end
+end
+
+export @expectErrors
+#Use to assert that n errors are expected at the point where macro appears
+#and deduct from error count
+macro expectErrors(n)
+  quote
+    if $(esc(_TESTCTX)).numFailed != $(esc(n))
+      println("Error: ", $(esc(n))," expected errors but ", $(esc(_TESTCTX)).numErrors, " actual")
+    else
+      $(esc(_TESTCTX)).numErrors -= $(esc(n))
+    end
+  end
+end
+
+export @testreport
+macro testreport()
+  quote
+    printTestReport($(esc(_TESTCTX)))
+  end
+end
+
+export @testcase
+#Create test case around a block
+macro testcase(block)
+  quote
+    let $(esc(_TESTCTX)) = JLTest.TestContext("No Name Test Case")
+      local tc = $(esc(_TESTCTX))
+      tc.setUpCase()
+      $(esc(block))
+      tc.tearDownCase()
+      if tc.numErrors == 0 && tc.numFailed == 0
+        println(tc.name, " Ok")
+        if tc.numSkipped > 0
+          println(tc.numSkipped, " Tests Skipped")
+        end
+      else
+        println(tc.name, " FAILED! ", tc.numFailed, " failures ", tc.numErrors, " errors")
+      end
+    end
+  end
+end
+
+export @test
+macro test(block)
+  quote
+    let tc = $(esc(_TESTCTX))
+      numRun = tc.numRun += 1
+      tc.curTest = "Test $numRun"
+      local before = tc.numFailed + tc.numErrors
+      tc.setUp()
+      try
+        $(esc(block))
+      catch ex
+        bt=catch_backtrace()
+        handleException(tc,tc.curTest,ex,bt)
+      end
+      tc.tearDown()
+      local after = tc.numFailed + tc.numErrors
+      if before == after
+        tc.numPassed +=1
+      else
+        println(tc.curTest, " Failed!")
+      end
+      tc.curTest = ""
+    end
+  end
+end
+
+export @testskip
+#Place in front of a test to unconditionally skip it
+macro testskip(args...)
+  local skip, blk
+  if length(args) == 1
+    skip = true
+    blk = args[1]
+  elseif length(args) == 2
+    (skip, blk) = args
+  else
+    error("@testskip accepts at most two arguments")
+  end
+
+  if skip
+    quote
+      $(esc(_TESTCTX)).numSkipped+=1
+    end
+  else
+      :(@test($(blk)))
+  end
+end
+
+end #JLTest
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,175 @@
 using JLTest
-using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@testcase begin
+
+  @casename "JLTest Tests"
+
+  x = 0
+
+  @setUp () -> (x+=1)
+  @tearDown () -> (x=0)
+
+  @test begin
+    @testname "assertEqual Tests"
+    @assertEqual(x,1)
+    @assertEqual(x,1)
+  end
+
+  @test begin
+    @testname "assertNotEqual Tests"
+    @assertNotEqual(x,0)
+  end
+
+  @test begin
+    @testname "assertLess Tests"
+    @assertLess(-1,1)
+  end
+
+  @test begin
+    @testname "assertLess Tests"
+    @assertLessEqual(-1,1)
+  end
+
+  @test begin
+    @testname "assertLessEqual Tests"
+    @assertLessEqual(1,1)
+  end
+
+  @test begin
+    @testname "assertGreater Tests"
+    @assertGreater(1,-1)
+  end
+
+  @test begin
+    @testname "assertGreaterEqual Tests"
+    @assertGreaterEqual(-1,-1)
+  end
+
+  myString1 = "a string"
+  myString1a = myString1
+  myString2 = "a string"
+  myArray1 = [1,2,3]
+  myArray1a = myArray1
+  myArray2 = copy(myArray1)
+
+  @test begin
+    @testname "assertIs Tests"
+
+    @assertIs(myString1, myString1a)
+    @assertIs(myArray1, myArray1a)
+  end
+
+  @test begin
+    @testname "assertIsNot Tests"
+    @assertIsNot(myString1, myString2)
+    @assertIsNot(myArray1, myArray2)
+  end
+
+  @test begin
+    @assertIn(2, [1,2,3])
+  end
+
+  @test begin
+    @assertNotIn(0, [1,2,3])
+  end
+
+  @test begin
+     @assertItemsEqual([3,2,1], [1,2,3])
+  end
+
+  @test begin
+    @assertIsA([3,2,1], Array)
+  end
+
+  @test begin
+    @assertIsA([3,2,1], Array{Int64,1})
+  end
+
+   @test begin
+     @assertIsNotA([3,2,1], Array{Int32,1})
+  end
+
+  @test begin
+    @assertMatches(r"a[0-9]+a", "a1234a")
+  end
+
+  @test begin
+    @assertNotMatches(r"a[0-3]+a", "a1234a")
+  end
+
+  @test begin
+    a = 2
+    b = 1
+    @assertTrue(a==2*b)
+  end
+
+  @testskip begin
+    @testname "uncond skip"
+    @testFailed("Should have been skipped")
+  end
+
+  @testskip true begin
+    @testname "skip with cond"
+    @testFailed("Should have been skipped")
+  end
+
+  @testreport
+
+#   #should fail
+#   @assertEqual(x,0)
+
+#   #should get DomainError
+#   @assertEqual(sqrt(-1),0)
+
+end
+
+
+# macroexpand(:(@testCase begin
+
+#   @testname "JLTest Tests"
+
+#   x = 0
+
+#   @setUp () -> (x+=1)
+#   @tearDown () -> (x=0)
+
+#   @assertEqual(x,1)
+
+#   @assertEqual(x,1)
+
+#   @assertEqual(x,0)
+
+# end))
+
+# macroexpand(:(@testCase begin
+#   myString1 = "a string"
+#   myString1a = myString1
+#   myString2 = copy(myString1)
+
+#   @assertIsNot(myString1, myString2)
+# end))
+
+# @testCase begin
+#   myString1 = "a string"
+#   myString1a = myString1
+#   myString2 = deepcopy(myString1)
+
+#   @assertIsNot(myString1, myString2)
+# end
+
+# macroexpand(:(@testCase begin
+
+#   @testCaseName "JLTest Tests"
+
+#   x = 0
+
+#   @setUp () -> (x+=1)
+#   @tearDown () -> (x=0)
+
+#   @test begin
+#     @testname "assertEqual Tests"
+#     @assertEqual(1,1)
+#   end
+# end
+# ))
+
